4b2e034eeeb5cea2f25f75921c014d09f8622789
Adelaniada <adadiromanova@yandex.ru>
2025-06-25 00:00:00 +0300

    Add files via upload

--- tst.js
+++ tst.js
@@ -1,11 +1,18 @@
 alert("Привет! Это моя тетрадь для отработки функций и прочих прикольностей.");
 
+
 //1
-document.getElementById('txt_1').innerHTML = myFunction(-10)
+
+let inputText = document.querySelector(".input-text")
+let send = document.querySelector(".send")
+
+document.getElementById('send-id').addEventListener('click', 
+    function() {
+        document.getElementById('txt_1').innerHTML = myFunction(inputText.value)
+    })
+
     function myFunction(a) {
         if (a > 0) {a = true}
         else {a = false}
         return a
     }
-
-//2
